3e6d7c116d8e01e7e8c0f60556e47bb15377fb88
Ahmadbekov Abdulholiq <abdulholiq571@gmail.com>
2024-09-26 21:46:32 +0500

    updated

--- src/routes/products/Products.jsx
+++ src/routes/products/Products.jsx
@@ -1,9 +1,11 @@
+import Hero from "@/components/hero/Hero";
 import ProductsComponent from "@/components/products/Products";
 import React from "react";
 
 const Products = () => {
   return (
     <>
+      <Hero />
       <ProductsComponent />
     </>
   );
